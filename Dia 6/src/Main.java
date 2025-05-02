4db8f840bb2cc01f97b66a769285382d184d2692
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-05-02 08:54:41 -0300

    Dia 6 completo

---
+++ Dia 6/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car("BMW", "X6", 2024, 125_000);
+
+        System.out.println(car.getMarca());
+        System.out.println(car.getModelo());
+        System.out.println(car.getAno());
+        System.out.println(car.valorVenda());
+    }
+}
